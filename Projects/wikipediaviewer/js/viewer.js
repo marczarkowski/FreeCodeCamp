9ebfff7a986c43658578b65afd5b8f306f57d6f5
marczarkowski <marcinczarkowski@onet.eu>
2016-12-14 08:57:42 +0100

    Working example AJAX request

--- Projects/wikipediaviewer/js/viewer.js
+++ Projects/wikipediaviewer/js/viewer.js
@@ -1,1 +1,5 @@
 "use strict";
+$(document).ready(function() {
+    let mediaWiki = "https:\/\/en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=prefixsearch&redirects=1&formatversion=2&exsentences=2&exlimit=9&exintro=1&explaintext=1&gpssearch=Albert+Einstein&gpslimit=9&callback=?";
+   $.getJSON(mediaWiki).done(function(data) { console.log(data) });
+});
