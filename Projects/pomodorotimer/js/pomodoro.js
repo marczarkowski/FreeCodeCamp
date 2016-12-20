4396abea952fb4ff743c58bb7fc76ad301dcb562
marczarkowski <marcinczarkowski@onet.eu>
2016-12-20 11:37:30 +0100

    Add next steps of develepoment (logic)

--- Projects/pomodorotimer/js/pomodoro.js
+++ Projects/pomodorotimer/js/pomodoro.js
@@ -1,4 +1,10 @@
-
+/* trzy tryby: inactive, work, break -
+kolor trybu inactive: #BDBDBD
+kolor trybu active: main: #00BCD4 bar: #00cfd4;
+kolor trybu break: main: #FF5722 bar: #ff5a26
+tryb wpływa na kolor przycisku play, pozostalego czasu, baru, kropki, tła
+do zrobienia: przetwarzanie czasu, dodanie dzwieków
+*/
 $(document).ready(function() {
     "use strict";
    $(".timeLeft").text(`${$(".sessionTime").text().trim()}:00`);
