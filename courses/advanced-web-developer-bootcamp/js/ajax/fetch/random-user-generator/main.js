17074c4419092a948e4924bc77831cdfe4bd6674
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-11-23 21:32:57 +0100

    Set thumbnail and fullname via AJAX

--- courses/advanced-web-developer-bootcamp/js/ajax/fetch/random-user-generator/main.js
+++ courses/advanced-web-developer-bootcamp/js/ajax/fetch/random-user-generator/main.js
@@ -6,25 +6,45 @@ btn.addEventListener('click', getUser);
 function getUser() {
   fetch(url)
     .then(handleErrors)
-    .then((data) => {
-      console.log(data);
-    })
+    .then(setUserProfile)
     .catch((err) => {
       console.log(err);
     });
 
-  function handleErrors(res) {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      return response.json().then(throwError);
+  function setUserProfile(data) {
+    const userData = data.results[0];
+
+    setFullName.call(userData);
+    setAvatar.call(userData);
+
+
+
+    function setFullName() {
+      const fullName = document.querySelector('#fullname');
+      const firstName = getCapitalizedName(this.name.first);
+      const lastName = getCapitalizedName(this.name.last);
+
+      fullName.textContent = `${firstName} ${lastName}`;
+
+      function getCapitalizedName(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+      }
     }
 
-    function throwError(data) {
-      let error = new Error(res.status)
-      error.response = data;
-      error.status = response.status;
-      throw Error;
+    function setAvatar() {
+      const avatar = document.querySelector('#avatar');
+
+      avatar.src = this.picture.thumbnail;
     }
+  }
+  function handleErrors(res) {
+    return (res.status === 200 ? res.json() : response.json().then(throwError));
+  }
+
+  function throwError(data) {
+    let error = new Error(res.status)
+    error.response = data;
+    error.status = response.status;
+    throw Error;
   }
 }
