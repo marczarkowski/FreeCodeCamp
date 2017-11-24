6da2ad8c787dd613b3de24f44c745069d6b08e7a
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-11-24 20:10:44 +0100

    Finish setUserProfile implementation

--- courses/advanced-web-developer-bootcamp/js/ajax/fetch/random-user-generator/main.js
+++ courses/advanced-web-developer-bootcamp/js/ajax/fetch/random-user-generator/main.js
@@ -6,45 +6,62 @@ btn.addEventListener('click', getUser);
 function getUser() {
   fetch(url)
     .then(handleErrors)
+    .then(parseJSON)
     .then(setUserProfile)
     .catch((err) => {
       console.log(err);
     });
 
-  function setUserProfile(data) {
-    const userData = data.results[0];
-
-    setFullName.call(userData);
-    setAvatar.call(userData);
-
-
-
-    function setFullName() {
-      const fullName = document.querySelector('#fullname');
-      const firstName = getCapitalizedName(this.name.first);
-      const lastName = getCapitalizedName(this.name.last);
-
-      fullName.textContent = `${firstName} ${lastName}`;
-
-      function getCapitalizedName(name) {
-        return name.charAt(0).toUpperCase() + name.slice(1);
-      }
-    }
+  function setUserProfile(user) {
+    setAvatar.call(user);
+    setCapitalizedTextContent('#fullname', user.name.first, user.name.last);
+    setTextContent('#username', user.login.username);
+    setTextContent('#email', user.email);
+    setCapitalizedTextContent('#city', user.location.city);
 
     function setAvatar() {
       const avatar = document.querySelector('#avatar');
 
-      avatar.src = this.picture.thumbnail;
+      avatar.src = this.picture.medium;
+    }
+
+    function setTextContent(selector, value) {
+      const node = document.querySelector(selector);
+
+      node.textContent = value
+    }
+
+    function setCapitalizedTextContent(selector, ...value) {
+      const node = document.querySelector(selector);
+
+      const splitBySpace = el => el.split(' ');
+
+      const flatten = (a, b) => a.concat(b);
+
+      const joinArrayWithCapitalization = (sum, cur) => `${sum}  ${getCapitalizedStr(cur)}`;
+
+      const getCapitalizedStr = str =>  str.charAt(0).toUpperCase() + str.slice(1);
+
+      node.textContent = value.map(splitBySpace)
+        .reduce(flatten)
+        .reduce(joinArrayWithCapitalization, '');
     }
   }
+
   function handleErrors(res) {
-    return (res.status === 200 ? res.json() : response.json().then(throwError));
+    return (res.status === 200 ? res : res.json().then(throwError));
+
+    function throwError(data) {
+      let error = new Error(res.status)
+      error.response = data;
+      error.status = response.status;
+      throw Error;
+    }
   }
 
-  function throwError(data) {
-    let error = new Error(res.status)
-    error.response = data;
-    error.status = response.status;
-    throw Error;
+  function parseJSON(res) {
+    return res.json().then((data) => data.results[0]);
   }
+
+
 }
