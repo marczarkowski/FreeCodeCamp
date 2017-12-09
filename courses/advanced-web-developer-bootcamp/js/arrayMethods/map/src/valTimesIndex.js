4ead559fe2ce670358b1dbb7eef6c93ae4b8d609
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-09 12:47:45 +0100

    Finish valTimesIndex exercise

---
+++ courses/advanced-web-developer-bootcamp/js/arrayMethods/map/src/valTimesIndex.js
@@ -0,0 +1,11 @@
+/*
+Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
+
+Examples:
+    valTimesIndex([1,2,3]) // [0,2,6]
+    valTimesIndex([1,-2,-3]) // [0,-2,-6]
+*/
+
+function valTimesIndex(arr) {
+  return arr.map((el, i) => el*i);
+}
