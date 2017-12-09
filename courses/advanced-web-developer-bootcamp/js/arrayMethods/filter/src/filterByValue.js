eacdabdd803d853d4e9d7f4307e2cccd533e4c19
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-09 13:00:58 +0100

    Finish filterByValue exercise

---
+++ courses/advanced-web-developer-bootcamp/js/arrayMethods/filter/src/filterByValue.js
@@ -0,0 +1,10 @@
+/*
+Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+
+Examples:
+    filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
+*/
+
+function filterByValue(arr, key){
+   return arr.filter(el => el.hasOwnProperty(key));
+}
