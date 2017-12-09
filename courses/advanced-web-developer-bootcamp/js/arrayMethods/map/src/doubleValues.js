d9955fe94fc322d4703f044d0f91290c2bc34b1b
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-09 12:46:16 +0100

    Finish doubleValues exercise

---
+++ courses/advanced-web-developer-bootcamp/js/arrayMethods/map/src/doubleValues.js
@@ -0,0 +1,11 @@
+/*
+Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+Examples:
+    doubleValues([1,2,3]) // [2,4,6]
+    doubleValues([1,-2,-3]) // [2,-4,-6]
+*/
+
+function doubleValues(arr) {
+  return arr.map(el => el*2);
+}
