72f120111c415dfffbda3c0002c20a9e739a765d
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-14 19:47:27 +0100

    Finish 'partition' exercise

---
+++ courses/advanced-web-developer-bootcamp/js/arrayMethods/reduce/src/partition.js
@@ -0,0 +1,37 @@
+/*
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
+
+Examples:
+
+    function isEven(val){
+        return val % 2 === 0;
+    }
+
+    var arr = [1,2,3,4,5,6,7,8];
+
+    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
+
+    function isLongerThanThreeCharacters(val){
+        return val.length > 3;
+    }
+
+    var names = ['Elie', 'Colt', 'Tim', 'Matt'];
+
+    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
+*/
+
+function partition(arr, callback) {
+  const partitionViaCallback = (acc, next) => {
+    if (callback(next)) {
+      acc[0].push(next);
+    } else {
+      acc[1].push(next);
+    }
+
+    return acc;
+  };
+
+  return arr.reduce(partitionViaCallback, [[], []]);
+}
+
+
