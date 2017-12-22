9067b3d202abe61c1571f22ac82c7f5c188ba900
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-22 19:48:46 +0100

    Refactor project to ES6

--- projects/vanilla-js/guess-the-password/app.js
+++ projects/vanilla-js/guess-the-password/app.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let password = '';
 
   const start = document.getElementById('start');
+  const toggleClasses = (element, ...args) => args.forEach(arg => element.classList.toggle(arg));
+
   start.addEventListener('click', () => {
-    const toggleClasses = (element, ...args) => args.forEach(arg => element.classList.toggle(arg));
+
 
     toggleClasses(document.getElementById('start-screen'), 'hide', 'show');
     toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
@@ -36,49 +38,53 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function shuffle(array) {
-    var arrayCopy = array.slice();
-    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+    const arrayCopy = [...array];
+
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
       // generate a random index between 0 and idx1 (inclusive)
-      var idx2 = Math.floor(Math.random() * (idx1 + 1));
+      const idx2 = Math.floor(Math.random() * (idx1 + 1));
 
       // swap elements at idx1 and idx2
-      var temp = arrayCopy[idx1];
-      arrayCopy[idx1] = arrayCopy[idx2];
-      arrayCopy[idx2] = temp;
+      [arrayCopy[idx1], arrayCopy[idx2]] = [arrayCopy[idx2], arrayCopy[idx1]];
     }
+
     return arrayCopy;
   }
 
   function setGuessCount(newCount) {
     guessCount = newCount;
-    document.getElementById("guesses-remaining").innerText = "Guesses remaining: " + guessCount + ".";
+    document.getElementById('guesses-remaining').innerText = `Guesses remaining: ${guessCount}.`;
   }
 
   function updateGame(e) {
-    if (e.target.tagName === "LI" && !e.target.classList.contains("disabled")) {
+    if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
       // grab guessed word, check it against password, update view
-      var guess = e.target.innerText;
-      var similarityScore = compareWords(guess, password);
-      e.target.classList.add("disabled");
-      e.target.innerText = e.target.innerText + " --> Matching Letters: " + similarityScore;
+      const guess = e.target.innerText;
+      const similarityScore = compareWords(guess, password);
+      e.target.classList.add('disabled');
+      e.target.innerText = `${e.target.innerText} --> Matching Letters: ${similarityScore}`;
       setGuessCount(guessCount - 1);
 
       // check whether the game is over
       if (similarityScore === password.length) {
-        toggleClasses(document.getElementById("winner"), 'hide', 'show');
+        toggleClasses(document.getElementById('winner'), 'hide', 'show');
         this.removeEventListener('click', updateGame);
       } else if (guessCount === 0) {
-        toggleClasses(document.getElementById("loser"), 'hide', 'show');
+        toggleClasses(document.getElementById('loser'), 'hide', 'show');
         this.removeEventListener('click', updateGame);
       }
     }
   }
 
   function compareWords(word1, word2) {
-    if (word1.length !== word2.length) throw "Words must have the same length";
-    var count = 0;
-    for (var i = 0; i < word1.length; i++) {
-      if (word1[i] === word2[i]) count++;
+    if (word1.length !== word2.length) {
+      throw Error('Words must have the same length');
+    }
+    let count = 0;
+    for (let i = 0; i < word1.length; i += 1) {
+      if (word1[i] === word2[i]) {
+        count += 1;
+      }
     }
     return count;
   }
