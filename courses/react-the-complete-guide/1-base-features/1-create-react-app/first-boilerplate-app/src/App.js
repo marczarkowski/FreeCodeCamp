15f8a912847531c860be6ade889e1e8d8c6ab4c9
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-29 21:05:56 +0100

    Create first functional component

--- courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
+++ courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm your first React App!</h1>
+        <Person name="Marcin Czarkowski" />
       </div>
     );
+    // const h1 = React.createElement('h1', null, 'I\'m your first React App!');
+    // return React.createElement('div', { className: 'App' }, h1);
   }
 }
 
---
+++ courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = (props) => {
+  return (
+      <div>
+        <p>I'm a {props.name}!</p>
+      </div>
+  );
+};
+
+export default person;
