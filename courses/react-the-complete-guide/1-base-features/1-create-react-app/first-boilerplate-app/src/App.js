860c360013a3b92372210773c659537bf1e64288
Marcin Czarkowski <marcinczarkowski@onet.eu>
2017-12-29 22:19:47 +0100

    Implement state to components

--- courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
+++ courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Marcin', age: 24 },
+      { name: 'Jędrzej', age: 24 },
+      { name: 'Michał', age: 25 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Marcin Konrad', age: 25 },
+        { name: 'Jędrzej', age: 24 },
+        { name: 'Michał', age: 26 },
+      ],
+    });
+  }
+
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm your first React App!</h1>
-        <Person name="Marcin Czarkowski" />
+        <h1>Hi, I'm your first React App!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: History</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
-    // const h1 = React.createElement('h1', null, 'I\'m your first React App!');
-    // return React.createElement('div', { className: 'App' }, h1);
   }
 }
 
