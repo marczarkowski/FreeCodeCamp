933e42c8180e3fcb7eae71391870fc4f7fd72233
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-01-02 20:34:09 +0100

    Implement two-way data binding

--- courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
+++ courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/App.js
@@ -11,24 +11,42 @@ class App extends Component {
     ],
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
         { name: 'Marcin Konrad', age: 25 },
-        { name: 'Jędrzej', age: 24 },
+        { name: newName, age: 24 },
         { name: 'Michał', age: 26 },
       ],
     });
-  }
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Marcin Konrad', age: 25 },
+        { name: event.target.value, age: 24 },
+        { name: 'Michał', age: 26 },
+      ],
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm your first React App!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: History</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Andrzejos')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Andrzej')}
+          changed={this.nameChangedHandler}>Hobbies: History</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
--- courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/Person/Person.js
+++ courses/react-the-complete-guide/1-base-features/1-create-react-app/first-boilerplate-app/src/Person/Person.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const person = (props) => {
   return (
-      <div>
-        <p>I'm {props.name} and I'm {props.age} years old!</p>
-        <p>{props.children}</p>
-      </div>
+    <div>
+      <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed}/>
+    </div>
   );
 };
 
