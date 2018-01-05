e0651c76eee328aa6ad53782275784be5218404d
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-01-05 20:10:13 +0100

    Create UserInput and UserOutput components

---
+++ courses/react-the-complete-guide/1-base-features/2-base-syntax-assignment/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+  return (
+    <div>
+      <p>Hej!</p>
+      <p>Jak się masz :)?</p>
+    </div>
+  );
+};
+
+export default userOutput;
