7f535985edbfe5b7840348512c26df758625e8b2
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-03-06 20:38:24 +0100

    Handle basic actions

--- courses/react-the-complete-guide/redux/src/containers/Counter/Counter.js
+++ courses/react-the-complete-guide/redux/src/containers/Counter/Counter.js
@@ -5,55 +5,34 @@ import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-  state = {
-    counter: 0
-  };
+  resultClickHandler = (id) => {
 
-  counterChangedHandler = (action, value) => {
-    switch (action) {
-      case "inc":
-        this.setState(prevState => {
-          return { counter: prevState.counter + 1 };
-        });
-        break;
-      case "dec":
-        this.setState(prevState => {
-          return { counter: prevState.counter - 1 };
-        });
-        break;
-      case "add":
-        this.setState(prevState => {
-          return { counter: prevState.counter + value };
-        });
-        break;
-      case "sub":
-        this.setState(prevState => {
-          return { counter: prevState.counter - value };
-        });
-        break;
-    }
   };
 
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
-          clicked={() => this.counterChangedHandler("inc")}
+          clicked={this.props.onIncrementCounter}
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={this.props.onDecrementCounter}
         />
-        <CounterControl
-          label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
-        />
+        <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.results.map(result => {
+            return <li style={{display: "inline-block", listStyleType: "none", fontSize: "18px", paddingRight: "5px"}} onClick={this.resultClickHandler}>{result}</li>;
+          })}
+        </ul>
       </div>
     );
   }
@@ -61,8 +40,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    results: state.results
   };
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({ type: "ADD", value: 5 }),
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 5 }),
+    onStoreResult: () => dispatch({ type: "STORE" })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- courses/react-the-complete-guide/redux/src/store/reducer.js
+++ courses/react-the-complete-guide/redux/src/store/reducer.js
@@ -1,9 +1,44 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "INCREMENT": {
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    }
+    case "DECREMENT": {
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    }
+    case "ADD": {
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    }
+    case "SUBTRACT": {
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    }
+    case "STORE": {
+      return {
+        ...state,
+       results: [...state.results, state.counter]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
 export default reducer;
